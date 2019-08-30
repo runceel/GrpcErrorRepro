a83d2bf4bd3b7e8c15d2c5a99f4bffc4b4ef2ce0
Kazuki Ota <k_ota28@hotmail.com>
2019-08-31 00:29:15 +0900

    init

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var c = new System.Net.Http.HttpClient
+            {
+                BaseAddress = new Uri("https://example.com"),
+            };
+
+            var s = Grpc.Net.Client.GrpcClient.Create<MyGrpcServices.HelloWorldService.HelloWorldServiceClient>(c);
+            var r = await s.SayHelloAsync(new MyGrpcServices.HelloWorldRequest { Name = "xxx" });
+            Console.WriteLine(r.Message);
+        }
+    }
+}
---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public async void HelloWorldButton_Click(object sender, RoutedEventArgs args)
+        {
+            var c = new System.Net.Http.HttpClient
+            {
+                BaseAddress = new Uri("https://example.com"),
+            };
+
+            var s = Grpc.Net.Client.GrpcClient.Create<MyGrpcServices.HelloWorldService.HelloWorldServiceClient>(c);
+            var r = await s.SayHelloAsync(new MyGrpcServices.HelloWorldRequest { Name = "xxx" });
+            MessageBox.Show(r.Message);
+        }
+    }
+}
